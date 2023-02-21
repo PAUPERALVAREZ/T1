dd4c780d7621931eb06f6e473f87f16bf9b8d99c
Pau Peralvarez <pau.peralvarez@estudiantat.upc.edu>
2023-02-21 11:31:47 +0100

    Codi + figures

--- ppc_codi.py
+++ ppc_codi.py
@@ -1,14 +1,14 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 import soundfile as sf
 
+
 ##EXERCICI 1
 
-##Freq = 4kHz
+##Freq = 800Hz
 T= 2.5                               # Durada de T segons
 fm=8000                              # Freqüència de mostratge en Hz
-fx=4000                               # Freqüència de la sinusoide
+fx=800                               # Freqüència de la sinusoide
 A=4                                  # Amplitud de la sinusoide
 pi=np.pi                             # Valor del número pi
 L = int(fm * T)                      # Nombre de mostres del senyal digital
@@ -16,6 +16,18 @@ Tm=1/fm                              # Període de mostratge
 t=Tm*np.arange(L)                    # Vector amb els valors de la variable temporal, de 0 a T
 x = A * np.cos(2 * pi * fx * t)      # Senyal sinusoidal
 sf.write('so_exemple1.wav', x, fm)   # Escriptura del senyal a un fitxer en format wav
+
+##Freq = 4kHz
+T= 2.5                               # Durada de T segons
+fm=8000                              # Freqüència de mostratge en Hz
+fx=4000                              # Freqüència de la sinusoide
+A=4                                  # Amplitud de la sinusoide
+pi=np.pi                             # Valor del número pi
+L = int(fm * T)                      # Nombre de mostres del senyal digital
+Tm=1/fm                              # Període de mostratge
+t=Tm*np.arange(L)                    # Vector amb els valors de la variable temporal, de 0 a T
+x = A * np.cos(2 * pi * fx * t)      # Senyal sinusoidal
+sf.write('so_exemple2.wav', x, fm)   # Escriptura del senyal a un fitxer en format wav
 
 ##Freq = 7kHz
 T= 2.5                               # Durada de T segons
@@ -27,5 +39,50 @@ L = int(fm * T)                      # Nombre de mostres del senyal digital
 Tm=1/fm                              # Període de mostratge
 t=Tm*np.arange(L)                    # Vector amb els valors de la variable temporal, de 0 a T
 x = A * np.cos(2 * pi * fx * t)      # Senyal sinusoidal
-sf.write('so_exemple1.wav', x, fm)   # Escriptura del senyal a un fitxer en format wav
+sf.write('so_exemple3.wav', x, fm)   # Escriptura del senyal a un fitxer en format wav
+ 
+
+##EXERCICI 2
+x_r, fm = sf.read('so_exemple2.wav')
+fx = 4000
+
+#Gràfic 5 periodes
+Tx=1/fx                                 # Període del senyal
+Ls=int(fm*5*Tx)                           # Nombre de mostres corresponents a 5 períodes de la sinusoide
+
+plt.figure(0)                             # Nova figura
+plt.plot(t[0:Ls], x_r[0:Ls])              # Representació del senyal en funció del temps
+plt.xlabel('t en segons')                 # Etiqueta eix temporal
+plt.title('5 periodes de la sinusoide')   # Títol del gràfic
+plt.show()                                # Visualització de l'objecte gràfic. 
+
+#FFT
+from numpy.fft import fft     # Importem la funció fft
+N=5000                        # Dimensió de la transformada discreta
+X=fft(x_r[0 : Ls], N)         # Càlcul de la transformada de 5 períodes de la sinusoide
+
+k=np.arange(N)                        # Vector amb els valors 0≤k<N
+
+plt.figure(1)                         # Nova figura
+plt.subplot(211)                      # Espai per representar el mòdul
+plt.plot(k,abs(X))                    # Representació del mòdul de la transformada
+plt.title(f'Transformada del senyal de Ls={Ls} mostres amb DFT de N={N}')   # Etiqueta del títol
+plt.ylabel('|X[k]|')                  # Etiqueta de mòdul
+plt.subplot(212)                      # Espai per representar la fase
+plt.plot(k,np.unwrap(np.angle(X)))    # Representació de la fase de la transformada, desenroscada
+plt.xlabel('Index k')                 # Etiqueta de l'eix d'abscisses 
+plt.ylabel('$\phi_x[k]$')             # Etiqueta de la fase en Latex
+plt.show()                            # Per mostrar els grafics
+
+##EXERCICI 3
+import math as m
+
+#Mòdul transformada en dB:
+X_dB = 20*m.log10(abs(X) / max(abs(X)))
+kk = fm/2 * np.arange(N)
+
+plt.figure(2)                         # Nova figura
+plt.plot(kk,X_dB)                    # Representació del mòdul de la transformada
+plt.title(f'Transformada del senyal de Ls={Ls} mostres amb DFT de N={N}')   # Etiqueta del títol
+plt.ylabel('|X[k]| en dB')                  # Etiqueta de mòdul
 
